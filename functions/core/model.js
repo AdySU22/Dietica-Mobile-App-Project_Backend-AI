a637439188c59910a472c057fa23b2979f098c3f
bryanrp <66724556+bryanrp@users.noreply.github.com>
2024-10-05 03:04:46 +0700

    Add basic model instruction

--- functions/core/model.js
+++ functions/core/model.js
@@ -3,4 +3,11 @@ const {GoogleGenerativeAI} = require("@google/generative-ai");
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-exports.model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+exports.model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: `
+    You are a diet and exercise advisor.
+    Only reply to diet and exercise topics.
+    Provide steps to improve diet and suggest exercises.
+  `,
+});
