d25b31819fa2989aa189517a99566ca489e6035d
bryanrp <66724556+bryanrp@users.noreply.github.com>
2024-10-05 03:06:12 +0700

    Add chatbot function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const {onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {chatbot} = require("./services/chatbot");
 
 exports.helloWorld = onCall((request) => {
   logger.info("Hello logs!", {structuredData: true});
   return "Hello from Firebase!";
 });
+
+exports.chatbot = chatbot;
---
+++ functions/services/chatbot.js
@@ -0,0 +1,57 @@
+const {onCall, HttpsError} = require("firebase-functions/v2/https");
+const {db} = require("../core/firestore");
+const {model} = require("../core/model");
+const logger = require("firebase-functions/logger");
+
+exports.chatbot = onCall(async (request) => {
+  const message = request.data.message;
+
+  if (!message || typeof message !== "string") {
+    throw new HttpsError("invalid-argument", "message must not empty");
+  }
+
+  // TODO enable auth
+  // if (!request.auth) {
+  //   throw new HttpsError("unauthenticated", "User not authenticated");
+  // }
+
+  // TODO where authId
+  const chatLogs = await db.collection("UserChatLog")
+      .orderBy("repliedAt")
+      .limit(5)
+      .get();
+  if (chatLogs) {
+    chatLogs.forEach((chatLog) => {
+      logger.info(chatLog.data());
+    });
+  }
+
+  const chat = await model.startChat(
+      {
+        history: chatLogs.docs.reduce((total, chatLog) => {
+          total.push(
+              {
+                role: "user",
+                parts: [{text: chatLog.data().message}],
+              },
+              {
+                role: "model",
+                parts: [{text: chatLog.data().reply}],
+              },
+          );
+          return total;
+        }, []),
+      },
+  );
+
+  const reply = await chat.sendMessage(message);
+
+  // TODO add authId
+  await db.collection("UserChatLog").add({
+    message: message,
+    reply: reply.response.text(),
+    repliedAt: new Date(),
+  });
+
+  return reply.response.text();
+});
