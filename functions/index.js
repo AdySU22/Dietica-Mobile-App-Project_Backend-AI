66a0cc797a738dddb203f5e65cf7ba7053dc31fe
bricoo <66724556+bryanrp@users.noreply.github.com>
2024-10-12 14:02:40 +0700

    Feature/chatbot (#1)
    * Add chatbot function
    
    * Implement getChatbot feature

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const {onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {getChatbot, sendChatbot} = require("./services/chatbot");
 
 exports.helloWorld = onCall((request) => {
   logger.info("Hello logs!", {structuredData: true});
   return "Hello from Firebase!";
 });
+
+exports.getChatbot = getChatbot;
+exports.sendChatbot = sendChatbot;
