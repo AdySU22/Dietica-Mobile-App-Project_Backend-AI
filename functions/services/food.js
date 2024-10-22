1651dee23f51d78fd80bced30d12b9f7bd1b55fb
bryanrp <66724556+bryanrp@users.noreply.github.com>
2024-10-22 10:34:04 +0700

    Update food endpoints request format

--- functions/services/food.js
+++ functions/services/food.js
@@ -2,10 +2,20 @@ const {onCall, HttpsError} = require("firebase-functions/v2/https");
 const {db} = require("../core/firestore");
 
 exports.getAllFood = onCall(async (req) => {
-  const {uid, page, limit} = req.data;
+  const {authId, page, limit} = req.data;
+
+  if (!authId) {
+    throw new HttpsError("invalid-argument", "authId is required");
+  }
+  if (page === null || page === undefined || typeof page !== "number") {
+    throw new HttpsError("invalid-argument", "page must not empty");
+  }
+  if (!limit || typeof limit !== "number") {
+    throw new HttpsError("invalid-argument", "limit must not empty");
+  }
 
   const foods = await db.collection("FoodLog")
-      .where("authId", "==", uid)
+      .where("authId", "==", authId)
       .orderBy("createdAt", "desc")
       .offset(page)
       .limit(limit)
@@ -18,12 +28,19 @@ exports.getAllFood = onCall(async (req) => {
 });
 
 exports.getFood = onCall(async (req) => {
-  const {uid, foodId} = req.data;
+  const {authId, foodId} = req.data;
+
+  if (!authId) {
+    throw new HttpsError("invalid-argument", "authId is required");
+  }
+  if (!foodId) {
+    throw new HttpsError("invalid-argument", "foodId is required");
+  }
 
   const foodDocRef = db.collection("FoodLog").doc(foodId);
   const foodDoc = await foodDocRef.get();
 
-  if (!foodDoc.exists || foodDoc.data().authId !== uid) {
+  if (!foodDoc.exists || foodDoc.data().authId !== authId) {
     throw new HttpsError("not-found", "Food not found");
   }
 
@@ -34,10 +51,17 @@ exports.getFood = onCall(async (req) => {
 });
 
 exports.createFood = onCall(async (req) => {
-  const {uid, food} = req.data;
+  const {authId, food} = req.data;
+
+  if (!authId) {
+    throw new HttpsError("invalid-argument", "authId is required");
+  }
+  if (!food) {
+    throw new HttpsError("invalid-argument", "food is required");
+  }
 
   const docRef = await db.collection("FoodLog").add({
-    authId: uid,
+    authId: authId,
     name: food.name,
     calories: food.calories,
     fat: food.fat,
@@ -61,12 +85,22 @@ exports.createFood = onCall(async (req) => {
 
 
 exports.updateFood = onCall(async (req) => {
-  const {uid, foodId, food} = req.data;
+  const {authId, foodId, food} = req.data;
+
+  if (!authId) {
+    throw new HttpsError("invalid-argument", "authId is required");
+  }
+  if (!foodId) {
+    throw new HttpsError("invalid-argument", "foodId is required");
+  }
+  if (!food) {
+    throw new HttpsError("invalid-argument", "food is required");
+  }
 
   const foodDocRef = db.collection("FoodLog").doc(foodId);
   const foodDoc = await foodDocRef.get();
 
-  if (!foodDoc.exists || foodDoc.data().authId !== uid) {
+  if (!foodDoc.exists || foodDoc.data().authId !== authId) {
     throw new HttpsError("not-found", "Food not found");
   }
 
@@ -93,12 +127,19 @@ exports.updateFood = onCall(async (req) => {
 
 
 exports.deleteFood = onCall(async (req) => {
-  const {uid, foodId} = req.data;
+  const {authId, foodId} = req.data;
+
+  if (!authId) {
+    throw new HttpsError("invalid-argument", "authId is required");
+  }
+  if (!foodId) {
+    throw new HttpsError("invalid-argument", "foodId is required");
+  }
 
   const foodDocRef = db.collection("FoodLog").doc(foodId);
   const foodDoc = await foodDocRef.get();
 
-  if (!foodDoc.exists || foodDoc.data().authId !== uid) {
+  if (!foodDoc.exists || foodDoc.data().authId !== authId) {
     throw new HttpsError("not-found", "Food not found");
   }
 
